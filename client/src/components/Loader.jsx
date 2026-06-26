8808c950ce3cb05cdb9dd7aa53172acc9d09c2cf
rutujdhodapkar <rutujdhodapkar@gmail.com>
2026-06-26 19:15:49 +0530

    Enlarge loader MetallicPaint container

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -27,7 +27,7 @@ export default function Loader({ onFinish }) {
         justifyContent: 'center',
       }}
     >
-      <div style={{ width: '80%', maxWidth: '500px', height: '180px' }}>
+      <div style={{ width: '90%', maxWidth: '800px', height: '280px' }}>
         <MetallicPaint
           imageSrc="/devcraft-logo.svg"
           seed={42}
